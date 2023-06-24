96141d6f101082db16d2c1adff47228b7c501c23
Nitsan.Maman <nit.maman@gmail.com>
2023-06-24 21:35:21 +0300

    Ready to deploy 3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { TasksContextProvider } from './context/TaskContext';
 import { AuthContextProvider } from './context/AuthContext';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
